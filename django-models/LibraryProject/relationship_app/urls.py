32925a56962f353edf25c34de75081a24e10ea6f
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-11-15 19:36:29 +0200

    2. Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,13 @@
 from . import views
 from .views import LibraryDetailView
 from .views import list_books
+from .views import registeration
 from django.urls import path
 from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     path('all_books',views.list_books,name='all_books'),
     path('all_books',LibraryDetailView.as_view(),name='all_books'),
+    path('signup/',registeration.as_view(),name='signup'),
     path('login',LoginView.as_view(template_name='relationship_app/user_registration/login.html'),name='logout'),
     path('logout',LogoutView.as_view(),name='logout')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm 
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth import login 
 # Create your views here.
 def list_books(request):
     books=Book.objects.all()
