def6121065f15e1f7a915f11204d37c74a5af7c4
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-11-20 01:28:43 +0200

    commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,14 +3,14 @@ from .views import LibraryDetailView
 from .views import list_books
 from django.urls import path
 from django.contrib.auth.views import LoginView,LogoutView
-from . import admin_view,librarian_view,member_view
+from . import Admin_view,librarian_view,member_view
 urlpatterns = [
     path('all_books/',views.list_books,name='all_books'),
     path('all_books/',LibraryDetailView.as_view(),name='all_books'),
     path('signup/',views.register,name='signup'),
     path('login/',LoginView.as_view(template_name='Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/User_Authentication/login.html'),name='logout'),
     path('logout/',LogoutView.as_view(template_name='Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/User_Authentication/login.html'),name='logout'),
-    path('Admin_view/',admin_view.Admin,name='Admin_view'),
+    path('Admin_view/',Admin_view.Admin,name='Admin_view'),
     path('Librarian_view/',librarian_view.librarian,name='Librarian_view'),
     path('Member_view/',member_view.member,name='Member_view'),
     path('add_book/',views.add_book_per,name='add_book_permission'),
