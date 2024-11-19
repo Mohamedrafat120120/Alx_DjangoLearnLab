65492fb69111042eb31563f553ae4c22530597f4
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-11-20 01:27:07 +0200

    commit

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -3,9 +3,9 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import user_passes_test
 
 
-def is_admin(user):
+def is_admin():
     return UserProfile.User=='Admin'
 
 @user_passes_test(is_admin)
-def admin(request):
+def Admin(request):
     return render(request,'admin.html')
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('signup/',views.register,name='signup'),
     path('login/',LoginView.as_view(template_name='Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/User_Authentication/login.html'),name='logout'),
     path('logout/',LogoutView.as_view(template_name='Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/User_Authentication/login.html'),name='logout'),
-    path('Admin_view/',admin_view.admin,name='Admin_view'),
+    path('Admin_view/',admin_view.Admin,name='Admin_view'),
     path('Librarian_view/',librarian_view.librarian,name='Librarian_view'),
     path('Member_view/',member_view.member,name='Member_view'),
     path('add_book/',views.add_book_per,name='add_book_permission'),
