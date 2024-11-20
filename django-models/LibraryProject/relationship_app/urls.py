55164c57c90095253722801590db8ebd45c17929
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-11-20 02:02:22 +0200

    commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('signup/',views.register,name='signup'),
     path('login/',LoginView.as_view(template_name='Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/User_Authentication/login.html'),name='logout'),
     path('logout/',LogoutView.as_view(template_name='Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/User_Authentication/login.html'),name='logout'),
-    path('Admin_view/',admin_view,name='Admin_view'),
-    path('Librarian_view/',librarian_view,name='Librarian_view'),
-    path('Member_view/',member_view,name='Member_view'),
+    path('Admin_view/',admin_view.Admin,name='Admin_view'),
+    path('Librarian_view/',librarian_view.librarian,name='Librarian_view'),
+    path('Member_view/',member_view.member,name='Member_view'),
     path('add_book/',views.add_book_per,name='add_book_permission'),
     path('edit_book/',views.add_book_per,name='add_book_permission'),
     path('delete_book/',views.add_book_per,name='add_book_permission')
