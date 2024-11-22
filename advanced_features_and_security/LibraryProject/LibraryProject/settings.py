79b8036fca5824d45675df530ce6aff2e53a4f4d
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-11-22 02:32:11 +0200

    0. Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from bookshelf.models import CustomUser
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,4 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/accounts/profile"
 LOGOUT_REDIRECT_URL = "/accounts/profile"
 
-AUTH_USER_MODEL=CustomUser
+AUTH_USER_MODEL='bookshelfCustomUser'
