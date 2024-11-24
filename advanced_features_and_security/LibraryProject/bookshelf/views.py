16abc141710cba75b3e69ec92a42c45ea91f50f1
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-11-25 01:54:10 +0200

    2. Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from django.contrib.auth.decorators import permission_required
-from bookshelf.forms import ExampleForm
+from .forms import ExampleForm
 # Create your views here.
 @permission_required('bookshelf.can_edit',raise_exception=True)
 def edit(request):
