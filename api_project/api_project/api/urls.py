4140d254e126a9be94336e54d8d5f24bf817543b
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-11-30 01:25:55 +0200

    1. Building Your First API Endpoint with Django REST Framework

--- api_project/api_project/api/urls.py
+++ api_project/api_project/api/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path,include
+from .views import BookList,BookViewSet
+from rest_framework.routers import DefaultRouter
+router=DefaultRouter
+router.register(r'books_all',BookViewSet,basename='Books_all')
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('books/', BookList.as_view(), name='book-list'),
+    path('',include(router.urls)),
 ]
--- api_project/api_project/api/views.py
+++ api_project/api_project/api/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,viewsets
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
 class BookList(generics.ListAPIView):
     data=Book.objects.all()
-    queryset = data
-    serializer_class=BookSerializer
+    
+    serializer=BookSerializer(data,many=True)
+    
+    
+
+
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer   
+    
