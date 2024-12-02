a5f4cff8282ad8f1d92b03746e5ff75ce0204217
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-12-03 01:13:44 +0200

    dsljfsd

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -83,6 +83,15 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
