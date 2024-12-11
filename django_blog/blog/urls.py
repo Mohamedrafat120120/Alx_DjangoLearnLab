49501b2e8995833ca83128259c7572c29a3064fe
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-12-11 02:20:46 +0200

    sdjflksak;ld

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView,LogoutView
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('register/',SignUpView.as_view(),name='register'),
+    path('register/',SignUpView.as_view(),name='blog/register.html'),
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
 ]
