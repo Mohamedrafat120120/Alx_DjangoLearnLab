84c6f3227d6353110222240347c1fb7d735a2629
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-12-13 01:30:40 +0200

    jsdfkjkl

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,7 +2,10 @@ from django.contrib.auth.views import LoginView,LogoutView
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('register/',SignUpView.as_view(),name='blog/register.html'),
+    path('register/',register.as_view(),name='register'),
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('profile/',profile.as_view(),name="profile"),
+    path('home/',home.as_view(),name="home"),
+    path('posts/',posts.as_view(),name="posts"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,24 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView,ListView,DeleteView,DetailView,UpdateView
+from django.views.generic import CreateView,ListView,DeleteView,DetailView,UpdateView,TemplateView
 from .models import post
-class SignUpView(CreateView):
+class register(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('blog/login')
     template_name = 'blog/register.html'
+    
+class profile(TemplateView):
+    template_name = 'blog/profile.html'
+    
+    
+class home(TemplateView):
+    template_name = 'blog/home.html'
+    
+    
+class posts(ListView):
+    model=post
+    template_name = 'blog/posts.html'
+    
     
     
 class display(ListView):
