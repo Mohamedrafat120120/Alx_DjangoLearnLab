a5e8adcaef79bdafded3d67307d64068f0654332
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-12-12 02:09:40 +0200

    skdfuijas

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,26 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-
+from django.views.generic import CreateView,ListView,DeleteView,DetailView,UpdateView
+from .models import post
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'blog/register.html'
+    
+    
+class display(ListView):
+    model=post
+    template_name='blog/display.html'
+class display(CreateView):
+    model=post
+    template_name='blog/display.html'
+class display(DetailView):
+    model=post
+    template_name='blog/display.html'
+class display(UpdateView):
+    model=post
+    template_name='blog/display.html'
+class display(DeleteView):
+    model=post
+    template_name='blog/display.html'
+    
