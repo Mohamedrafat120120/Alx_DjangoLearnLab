4c99ed455fe3b1929fcc9dee00bbbec1208c705e
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2025-01-12 23:46:21 +0200

    dfsklj

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,5 +9,7 @@ comment_router.register(r'Commit',view_comments)
 urlpatterns = [
     path('view-user-posts/',include(router.urls)),
     path('view-user-comments/',include(comment_router.urls)),
-    path('feed/<int:user_id>/',feed.as_view(),name="feed")
+    path('feed/<int:user_id>/',feed.as_view(),name="feed"),
+    path('<int:pk>/like/'),
+    path('<int:pk>/unlike/'),
 ]
